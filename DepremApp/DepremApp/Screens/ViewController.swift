9c974086b050ddf41d096b1def91af0d58606ed8
onurduyar <onurdyr510@gmail.com>
2023-05-16 16:18:24 +0300

    project was initialized.

---
+++ DepremApp/DepremApp/Screens/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  DepremApp
+//
+//  Created by Onur Duyar on 16.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+}
+
