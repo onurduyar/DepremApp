1b0db2e55789d5ba5db02b1def1d3ee1beec19b4
onurduyar <onurdyr510@gmail.com>
2023-05-16 17:18:36 +0300

    tabbarcontroller was added

---
+++ DepremApp/DepremApp/Screens/TabBarController.swift
@@ -0,0 +1,40 @@
+//
+//  TabBarController.swift
+//  Deprem
+//
+//  Created by Onur Duyar on 16.04.2023.
+//
+
+import UIKit
+
+final class TabBarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tabBar.backgroundColor = .white
+        tabBar.tintColor = .black
+      
+        let firstVC = EarthQuakeListVC()
+        let firstNavVC = UINavigationController(rootViewController: firstVC)
+        firstNavVC.title = "Deprem Listesi"
+        firstNavVC.tabBarItem.image = UIImage(systemName: "info.bubble")
+        firstNavVC.tabBarItem.selectedImage = UIImage(systemName: "info.bubble.fill")
+        
+    
+        let secondVC = MapVC()
+        let secondNavVC = UINavigationController(rootViewController: secondVC)
+        secondNavVC.title = "Isı Haritası"
+        secondNavVC.tabBarItem.image = UIImage(systemName: "map")?.withRenderingMode(.alwaysOriginal)
+        secondNavVC.tabBarItem.selectedImage = UIImage(systemName: "map.fill")?.withRenderingMode(.alwaysOriginal)
+        
+    
+        let thirdVC = EarthQuakeListVC()
+        let thirdNavVC = UINavigationController(rootViewController: thirdVC)
+        thirdNavVC.title = "Yardım"
+        thirdNavVC.tabBarItem.image = UIImage(systemName: "cross.circle")
+        thirdNavVC.tabBarItem.selectedImage = UIImage(systemName: "cross.circle.fill")
+    
+        self.viewControllers = [firstNavVC, secondNavVC, thirdNavVC]
+        self.selectedIndex = 1
+    }
+}
